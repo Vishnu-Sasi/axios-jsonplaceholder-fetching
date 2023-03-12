258e27d26da3fcfdb3040b550f2431d26ee577f2
vishnusasi7559 <vishnumkuttiady982@gmail.com>
2023-03-12 18:29:12 +0530

    json api get and post request using axios

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,35 +5,46 @@ import { useState, useEffect } from "react";
 
 const Body = () => {
   const [users, setUsers] = useState([]);
-  const handleChange =(e)=>{
-    console.log(e.target.value)
- }
- const handleClick =()=>{
+  const defaultVal = {
+    name: "",
+  };
+  const [post, setPost] = useState(defaultVal);
 
- }
-  const usersList = () => {
-    
+  const URL = "https://jsonplaceholder.typicode.com";
+  const handleChange = (e) => {
+    setPost({ ...post, [e.target.name]: e.target.value });
+  };
+  const addUser = () => {
     axios
-      .get("https://jsonplaceholder.typicode.com/users")
+      .post(`${URL}/users`, post)
+      .then((response) => {
+        setUsers([...users,response.data])
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const usersList = () => {
+    axios
+      .get(`${URL}/users`)
       .then((response) => {
         setUsers(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
-      
   };
- 
+
   useEffect(() => {
     usersList();
-    
-  },[]);
-  
+  }, []);
+
   return (
     <>
       <div className="wrapper">
-        <input type="text" onChange={handleChange}/>
-        <button onClick={handleClick}>add users</button>
+        <input type="text" name="name" onChange={handleChange} />
+        <button onClick={addUser}>add users</button>
       </div>
       {users.map((ele) => {
         return (
